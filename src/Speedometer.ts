7a309a8f96b7a8c8408ad25fe6e6b01af3af1e7b
Francisco Castro <franciscommcastro@gmail.com>
2021-10-12 22:23:55 -0300

    Adding referencial consideration for simplicity

--- src/Speedometer.ts
+++ src/Speedometer.ts
@@ -19,7 +19,11 @@ export default class Speedometer {
     const { x, y, z } = accelerometerData;
 
     // Extract each axis value and convert to m/s^2
-    const acc = [x, y, z].map((component) => component * Constants.G);
+    const acc = [x, y, z - 1].map((component) => component * Constants.G);
+
+    // Here we will consider dispositive on horizontal for simplicity
+    // TODO remove (0,0,g) based on gyroscope data to consider earth-dispositive referential change
+    acc[2] -= Constants.G;
 
     // Append to array of values
     this.measuredData.push(acc);
